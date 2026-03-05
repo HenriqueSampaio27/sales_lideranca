f0c439137d988ff52ff586c3e32a1f1425428812
Pedro Henrique Sampaio <henriquesampaioabreu123@gmail.com>
2026-03-05 11:25:13 -0300

    configuracões do deploy

--- vite.config.ts
+++ vite.config.ts
@@ -25,6 +25,7 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
-      }
+      },
+      base: "/sales_lideranca",
     };
 });
