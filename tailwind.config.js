81bf02a35b84438751d8c038807437da9c0f919b
Pedro Henrique Sampaio <henriquesampaioabreu123@gmail.com>
2026-03-06 17:15:07 -0300

    config tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: "class",
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+            "primary": "#ffd900",
+            "background-dark": "#0f0f0f",
+            "surface-dark": "#1a1a1a",
+            "border-dark": "#2d2d2d",
+            "accent-dark": "#2a2a2a",
+        },
+        fontFamily: {
+            sans: ['Inter', 'sans-serif'],
+        }
+    },
   },
   plugins: [],
 }
