c91c4fee181a709bb3c7d524c472a6bb73bc2569
Pedro Henrique Sampaio <henriquesampaioabreu123@gmail.com>
2026-03-07 09:55:12 -0300

    correcting routes

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ export default {
   darkMode: "class",
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
