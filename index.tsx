f4e8765e0c2a063ed5e7a3ba747e5c6752c6c9c5
Pedro Henrique Sampaio <henriquesampaioabreu123@gmail.com>
2026-03-07 10:38:21 -0300

    correcting routes

--- index.tsx
+++ index.tsx
@@ -1,8 +1,9 @@
 
+import "./index.css"
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import "./index.css"
+
 //atualizando...
 const rootElement = document.getElementById('root');
 if (!rootElement) {
