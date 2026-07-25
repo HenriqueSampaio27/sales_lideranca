f79e49b45ed345cb1a8f9cd921e11c2ec38fec97
Pedro Henrique Sampaio <henriquesampaioabreu123@gmail.com>
2026-07-25 10:04:25 -0300

    move the file layout.tsx and file sidebar.tsx to the folder layouts

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { HashRouter, Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import Login from "./src/pages/Login";
-import Layout from "./src/components/Layout";
+import Layout from "./src/layouts/Layout";
 import PrivateRoute from "./src/routes/PrivateRoute";
 import { appRoutes } from "./routes/routes";
 import { ComponentType } from "react";
